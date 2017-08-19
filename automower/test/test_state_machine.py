0a1ffcb71264ce2eec1a4fce5c24101722be49b6
aasmune <aasm.eek@gmail.com>
2017-08-19 13:05:12 +0200

    Added first unit test for state machine, updated requirements with tox

--- automower/test/test_state_machine.py
+++ automower/test/test_state_machine.py
@@ -1,9 +1,16 @@
 import pytest
+from automower.state_machine import AutomowerStateMachine
 
 class TestStateMachine(object):
 
-    def test_that_passes(self):
-        assert True
-    
-    def test_that_fails(self):
-        assert True
+    def test_correct_states_after_initialiation(self):
+
+        class TestAutomowerStateMachineInitialization(AutomowerStateMachine):
+            def _update_actual_mower_status(self):
+                # Hardcoded status for testing.
+                self.actual_automower_status = AutomowerStateMachine.STATE_MOWING
+
+        machine = TestAutomowerStateMachineInitialization()
+        assert len(machine.states) == len(AutomowerStateMachine.STATES)
+        assert machine.state == AutomowerStateMachine.STATE_MOWING
+        
